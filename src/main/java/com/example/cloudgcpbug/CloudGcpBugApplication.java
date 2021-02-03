ce0865bedae48917b832c375c22902da0e8ed080
gbalmat <gabalmat@ncsu.edu>
2021-02-03 18:09:16 -0500

    Init commit

---
+++ src/main/java/com/example/cloudgcpbug/CloudGcpBugApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudgcpbug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudGcpBugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGcpBugApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cloudgcpbug/CloudGcpBugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudgcpbug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGcpBugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
